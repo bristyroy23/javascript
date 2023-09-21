b497c41c08f989e3d1c54f313f5c51f6ed76590d
bristy <bristyroy651@gmail.com>
2023-09-21 09:57:15 +0600

    first commmit

--- loop.js
+++ loop.js
@@ -12,3 +12,11 @@ for(let i = 1; i<=10; i++){
         console.log(`2 * ${i} = &{2 * i}`)
     }
 }
+for(let i = 1; i<=10; i++){
+    if(1 % 2 == 0){
+        console.log(`2 * ${i} = &{2 * i}`)
+    }
+    else{
+        console.log("i is odd")
+    }
+}
